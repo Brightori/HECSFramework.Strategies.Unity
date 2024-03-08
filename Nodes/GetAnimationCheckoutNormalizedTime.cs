ff46e5b41642af276c0d68b50ec69ec532640a8e
Brightori <el.brightori@gmail.com>
2024-03-09 01:02:23 +0300

    add: nodes around animations

---
+++ Nodes/GetAnimationCheckoutNormalizedTime.cs
@@ -0,0 +1,15 @@
+﻿using Components;
+using HECSFramework.Core;using HECSFramework.Unity;
+using UnityEngine;
+
+namespace Strategies{	[Documentation(Doc.Strategy, "this node provide normalized timing from AnimationCheckOutsHolderComponent")]	public class GetAnimationCheckoutNormalizedTime : GenericNode<float>	{
+        [Connection(ConnectionPointType.In, "<Entity> Animator Owner")]
+        public GenericNode<Entity> AnimatorOwner;
+
+        [ExposeField]		public AnimationEventIdentifier AnimationEventID;				[Connection(ConnectionPointType.Out, "<float> Out")]		public BaseDecisionNode Out;				public override string TitleOfNode { get; } = "GetAnimationCheckoutNormalizedTime";				public override void Execute(Entity entity)		{		}				public override float Value(Entity entity)		{			var needed = AnimatorOwner != null ? AnimatorOwner.Value(entity) : entity;			if (needed.TryGetComponent(out AnimationCheckOutsHolderComponent animationCheckOutsHolderComponent))
+			{
+				if (animationCheckOutsHolderComponent.TryGetCheckoutInfo(AnimationEventID, out var checkoutInfo)) 
+				{
+					return checkoutInfo.TimingNormalized;
+				}
+			}			Debug.LogWarning("we dont have checkout holder on " + entity.ContainerID);			return 0;		}	}}
---
+++ Nodes/GetCurrentAnimationNormalizedTimeNode.cs
@@ -0,0 +1,39 @@
+﻿using Components;
+using HECSFramework.Core;
+
+namespace Strategies
+{
+    public class GetCurrentAnimationNormalizedTimeNode : GenericNode<float>
+    {
+        public override string TitleOfNode { get; } = "GetCurrentAnimationNormalizedTimeNode";
+
+        [Connection(ConnectionPointType.In, "<Entity> Animator Owner")]
+        public GenericNode<Entity> AnimatorOwner;
+
+        [Connection(ConnectionPointType.Out, "<float>")]
+        public BaseDecisionNode Out;
+
+        [ExposeField]
+        public int AnimationLayer;
+
+        public override void Execute(Entity entity)
+        {
+        }
+
+        public override float Value(Entity entity)
+        {
+            var check = AnimatorOwner == null ? entity : AnimatorOwner.Value(entity);
+
+            if (check.TryGetComponent(out AnimatorStateComponent animatorStateComponent))
+            {
+                var currentState = animatorStateComponent.Animator.GetCurrentAnimatorStateInfo(AnimationLayer);
+                return currentState.normalizedTime;
+            }
+            else
+            {
+                HECSDebug.LogWarning($"{entity.ID} {entity.GUID} doent have animator state component");
+                return 0;
+            }
+        }
+    }
+}
